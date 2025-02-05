aae348f025958e742641dcc3d383ed79dd88100c
Haletran <bpasquier123@gmail.com>
2025-02-05 14:21:04 +0100

    feat: automatically run android emulator

--- flake.nix
+++ flake.nix
@@ -7,10 +7,17 @@ stdenv.mkDerivation {
     ];
     shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
-        if [ ! -d "swifty/node_modules" ]; then
-            cd swifty && npm install
+        export ANDROID_HOME=$HOME/Android/Sdk
+        export PATH=$PATH:$ANDROID_HOME/emulator
+        export PATH=$PATH:$ANDROID_HOME/platform-tools
+        cd swifty && npm install
+        if [ ! -f .env ]; then
+            read -p "Enter CLIENT_ID: " CLIENT_ID
+            read -p "Enter CLIENT_SECRET: " CLIENT_SECRET
+            echo "EXPO_PUBLIC_CLIENT_ID=$CLIENT_ID" >> .env
+            echo "EXPO_PUBLIC_CLIENT_SECRET=$CLIENT_SECRET" >> .env
         fi
-        #eas build --platform android --profile preview
-        npm run web
+        npm run android2
+        #npx eas build --platform android --profile preview
     '';
 }
